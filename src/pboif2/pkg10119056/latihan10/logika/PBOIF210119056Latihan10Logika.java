57df5322000655ba3b93741866b46004619b1676
Irfan <>
2020-10-16 19:39:19 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan10/logika/PBOIF210119056Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan10.logika;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan Logika
+ */
+
+public class PBOIF210119056Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa=(umur>=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
